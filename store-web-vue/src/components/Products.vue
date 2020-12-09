e7de682a02cfc136bddee19bd8d09b6d0f51c36c
Suntisuk Khemphed <suntisuk.go@gmail.com>
2020-12-09 15:22:39 +0700

    Install bootstrap version 4.5

--- store-web-vue/src/components/Products.vue
+++ store-web-vue/src/components/Products.vue
@@ -1,24 +1,29 @@
 <template>
-    <div>
-        <p id="productList">Toys</p>
-    </div>
-    <div>
-        <div>
-            <label for="inputAge">Select age</label>
-            <select id="inputAge" v-model="selected">
-                <option value="aSelect" disabled="disabled" >Choose</option>
-                <option value="a1">less 6 m</option>
-                <option value="a2">7-12 m</option>
-                <option value="a3">1-2 y</option>
-                <option value="a4">3-6 y</option>
-                <option value="a5">more 6 y</option>
-                <option value="a6">All ages</option>
-            </select>
-        </div>
-        <div>
-            <label for="inputGender">Select gender</label>
+    <div class="container">
 
+        <div class="row">
+            <p id="productList">Toys</p>
         </div>
+
+        <div class="row">
+            <div class="col">
+                <label for="inputAge">Select age</label>
+                <select id="inputAge" v-model="selected">
+                    <option value="aSelect" disabled="disabled" >Choose</option>
+                    <option value="a1">less 6 m</option>
+                    <option value="a2">7-12 m</option>
+                    <option value="a3">1-2 y</option>
+                    <option value="a4">3-6 y</option>
+                    <option value="a5">more 6 y</option>
+                    <option value="a6">All ages</option>
+                </select>
+            </div>
+            <div class="col">
+                <label for="inputGender">Select gender</label>
+
+            </div>
+        </div>
+        
     </div>
 </template>
 
