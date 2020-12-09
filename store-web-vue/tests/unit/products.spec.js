20d2b621f329ea975d390d9dde304ff9da9c7425
Suntisuk Khemphed <suntisuk.go@gmail.com>
2020-12-09 13:02:44 +0700

    Add All option age, Create Select gender Label

--- store-web-vue/tests/unit/products.spec.js
+++ store-web-vue/tests/unit/products.spec.js
@@ -14,7 +14,7 @@ describe('Products', () => {
     expect(wrapper.find('#productList').text()).toBe(expectLabelToy)
   })
 
-  it('Create Select age element', async () => {
+  it('Create Select age Label', async () => {
     const expectSelectAge = 'Select age'
 
     const wrapper = mount(Products)
@@ -58,5 +58,67 @@ describe('Products', () => {
     expect(select.element.value).toBe(expectValueAge)
     expect(select.find('option:checked').element.text).toBe(expectTextAge)
     expect(select.find('option:checked').element.selected).toBe(true)
+  })
+
+  it('Change Option age by text is 7-12 m', async () => {
+    const expectValueAge = 'a2'
+    const expectTextAge = '7-12 m'
+
+    const wrapper = mount(Products)
+    const select = wrapper.find(`#${selectAgeId}`)
+    await select.setValue(expectValueAge)
+
+    expect(select.element.value).toBe(expectValueAge)
+    expect(select.find('option:checked').element.text).toBe(expectTextAge)
+    expect(select.find('option:checked').element.selected).toBe(true)
+  })
+
+  it('Change Option age by text is 3-6 y', async () => {
+    const expectValueAge = 'a4'
+    const expectTextAge = '3-6 y'
+
+    const wrapper = mount(Products)
+    const select = wrapper.find(`#${selectAgeId}`)
+    await select.setValue(expectValueAge)
+
+    expect(select.element.value).toBe(expectValueAge)
+    expect(select.find('option:checked').element.text).toBe(expectTextAge)
+    expect(select.find('option:checked').element.selected).toBe(true)
+  })
+
+  it('Change Option age by text is more 6 y', async () => {
+    const expectValueAge = 'a5'
+    const expectTextAge = 'more 6 y'
+
+    const wrapper = mount(Products)
+    const select = wrapper.find(`#${selectAgeId}`)
+    await select.setValue(expectValueAge)
+
+    expect(select.element.value).toBe(expectValueAge)
+    expect(select.find('option:checked').element.text).toBe(expectTextAge)
+    expect(select.find('option:checked').element.selected).toBe(true)
+  })
+
+  it('Change Option age by text is All ages', async () => {
+    const expectValueAge = 'a6'
+    const expectTextAge = 'All ages'
+
+    const wrapper = mount(Products)
+    const select = wrapper.find(`#${selectAgeId}`)
+    await select.setValue(expectValueAge)
+
+    expect(select.element.value).toBe(expectValueAge)
+    expect(select.find('option:checked').element.text).toBe(expectTextAge)
+    expect(select.find('option:checked').element.selected).toBe(true)
+  })
+
+  const selectGenderId = 'inputGender'
+
+  it('Create Select gender Label', async () => {
+    const expectSelectGender = 'Select gender'
+
+    const wrapper = mount(Products)
+
+    expect(wrapper.find(`[for="${selectGenderId}"]`).text()).toBe(expectSelectGender)
   })
 })
